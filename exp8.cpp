d639735537360d29736ccff5da54ac4a18f7e521
Jatin Rai <jatinrai198@gmail.com>
2018-11-14 21:20:26 +0530

    Update exp8.cpp

--- exp8.cpp
+++ exp8.cpp
@@ -1,4 +1,6 @@
-
+/* Write a program to accept N numbers in an array, and then sort the array using Insertion Sort. 
+Then accept a number from the user and insert it in the array according to the sequential order
+*/
 #include<iostream>
 using namespace std;
 void insertionsort(int a[],int n)
