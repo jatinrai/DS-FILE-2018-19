1a5da0585e6717e93e23e584975c833273fb0323
Jatin Rai <jatinrai198@gmail.com>
2018-11-14 21:17:50 +0530

    Update exp6.cpp

--- exp6.cpp
+++ exp6.cpp
@@ -1,4 +1,6 @@
-
+/* Write a program to demonstrate the use of stack in converting arithmetic expression
+from infix notation to postfix notation and in evaluating arithmetic postfix expression.
+*/
     #include<iostream>
     using namespace std;
 
