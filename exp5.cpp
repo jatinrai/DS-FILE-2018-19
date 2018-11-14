eb1543d0c52a96479ef05ef1ed3006bcbea08383
Jatin Rai <jatinrai198@gmail.com>
2018-11-14 21:16:59 +0530

    Update exp5.cpp

--- exp5.cpp
+++ exp5.cpp
@@ -1,4 +1,9 @@
-
+/* Write a menu driven program that implements the following operations on a   
+Stack(either implement as Linear array and as Linked list) :
+•	Push 
+•	Pop  
+•	Display Top of the stack
+*/
 #include<bits/stdc++.h>
 using namespace std;
 #define MAX 6
