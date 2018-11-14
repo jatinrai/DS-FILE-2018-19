feb1d55ebbcf976083b65c73b411ae6f523f4a04
Jatin Rai <jatinrai198@gmail.com>
2018-11-14 21:16:18 +0530

    Update Exp4(Doubly).cpp

--- Exp4(Doubly).cpp
+++ Exp4(Doubly).cpp
@@ -1,3 +1,10 @@
+/* Write a menu driven program that implements the following operations on a   
+doubly linked list :
+•	Insert a new element at the beginning ,end and in-between the given list
+•	Delete an existing element
+•	Search an element
+•	Display all the elements 
+*/
 #include<iostream>
 using namespace std;
 struct node {
